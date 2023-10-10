859d7bbb85f8ac3c92e15fd147f78de2320e4796
troyamanda7@gmail.com <troyamanda7@gmail.com>
2023-10-10 00:12:04 -0300

    ETIQUETAS PARA UN CÓDIGO LEGIBLE

--- src/nav.js
+++ src/nav.js
@@ -1,3 +1,4 @@
+//FUNCION PARA QUE APAREZCA Y DESAPAREZCA EL NAV PARA CELU, ADEMAS LE DA LA ANIMACIÓN 
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "flex") {
