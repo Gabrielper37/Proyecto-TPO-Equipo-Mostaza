2a0d6a1521662705189758dbfa9e5891f8156d69
Lucas <troyamanda7@gmail.com>
2023-10-25 20:37:59 -0300

    boton volver arriba

--- src/nav.js
+++ src/nav.js
@@ -8,4 +8,13 @@ function myFunction() {
       x.className = "animate__animated animate__fadeIn";
     }
   }
-  // When the user scrolls the page, execute myFunction
+
+$(document).ready(function(){ irArriba(); }); //Hacia arriba
+
+function irArriba(){
+  $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+}
