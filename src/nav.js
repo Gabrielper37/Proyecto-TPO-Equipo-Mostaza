7e48ec02a316cfb03a767f64f6c5dafface9ef33
Lucas <troyamanda7@gmail.com>
2023-10-25 22:57:15 -0300

    rammstein sticky random con animacion

--- src/nav.js
+++ src/nav.js
@@ -18,3 +18,53 @@ function irArriba(){
   });
   $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
 }
+
+
+/**
+    * Array con las imagenes que se iran mostrando en la web
+    */
+var imagenes=new Array(
+  'https://rammwiki.net/www/w/images/b/b8/RunesLogo1994.png',
+  'https://rammwiki.net/www/w/images/9/9e/1994Logo_Fill.png',
+  'https://rammwiki.net/www/w/images/0/0d/RammsteinNewspaperLogo.png',
+  'https://rammwiki.net/www/w/images/b/b5/1995Logo.png',
+  'https://rammwiki.net/www/w/images/a/aa/TrialByFireLogo.png',
+  'https://rammwiki.net/www/w/images/d/d4/1994Logo_Outline.png',
+  'https://rammwiki.net/www/w/images/3/39/2001Logo.png',
+  'https://rammwiki.net/www/w/images/8/8b/5TrackDemoSpineLogo.png',
+  'https://rammwiki.net/www/w/images/f/f1/2011logo.png',
+  'https://rammwiki.net/www/w/images/8/82/2012logo.png',
+
+);
+
+/**
+* Funcion para cambiar la imagen
+*/
+function rotarImagenes()
+{
+  // obtenemos un numero aleatorio entre 0 y la cantidad de imagenes que hay
+  var index=Math.floor((Math.random()*imagenes.length));
+
+  // cambiamos la imagen
+  document.getElementById("imagen").src=imagenes[index];
+  document.getElementById("imagen").animate(
+    {
+      opacity: [0, 0.5, 1],
+      offset: [0, 0.8], // Shorthand for [ 0, 0.8, 1 ]
+      easing: ["ease-in", "ease-out"],
+    },
+    2000,
+  );
+}
+
+/**
+* Función que se ejecuta una vez cargada la página
+*/
+onload=function()
+{
+  // Cargamos una imagen aleatoria
+  rotarImagenes();
+
+  // Indicamos que cada 5 segundos cambie la imagen
+  setInterval(rotarImagenes,15000);
+}
