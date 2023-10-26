44b3bd568ee60444792c3dafa72457281b9cd84b
unknown <mariano.gabriel.persano@gmail.com>
2023-10-25 21:41:56 -0300

    UpdateNav y agregada flecha By Lucas

--- src/nav.js
+++ src/nav.js
@@ -1,8 +1,20 @@
+//FUNCION PARA QUE APAREZCA Y DESAPAREZCA EL NAV PARA CELU, ADEMAS LE DA LA ANIMACIÓN 
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "flex") {
       x.style.display = "none";
     } else {
       x.style.display = "flex";
+      x.className = "animate__animated animate__fadeIn";
     }
   }
+
+$(document).ready(function(){ irArriba(); }); //Hacia arriba
+
+function irArriba(){
+  $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+}
