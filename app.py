097b51ed8d0d015ed87e64437e3078596ae0cde3
unknown <mariano.gabriel.persano@gmail.com>
2023-11-30 19:38:20 -0300

    Preinicializado de paginas en app.py

--- app.py
+++ app.py
@@ -16,14 +16,36 @@ mysql.init_app(app)
 
 @app.route("/")
 
+def Index():
+    return render_template("index.html")
+
+@app.route("/Discografia")
+def Discografia():
+   return render_template("Discografia.html")
+
+@app.route("/Historia")
+def Historia():
+   return render_template("Historia.html")
+
+@app.route("/Nosotros")
+def Nosotros():
+   return render_template("Nosotros.html")  
+
+@app.route("/Login")
 def Login():
     sql = "INSERT INTO `tpocrud`.`loginsystem` (`ID`, `User`, `Password`, `Email`, `Rank`) VALUES (NULL, 'Gabriel', 'Mariano', 'mariano.gabriel.persano@gmail.com', '1');"
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
     conn.commit()
-
     return render_template("Login.html")
+
+
+@app.route("/AdminNosotros")
+def AdminNosotros():
+
+    return render_template("AdminNosotros.html")
+
 
 
 if __name__=="__main__":
